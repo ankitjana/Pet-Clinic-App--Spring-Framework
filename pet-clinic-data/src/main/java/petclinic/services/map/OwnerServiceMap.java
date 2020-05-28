be45a57a12060272a372d99d5b0ff2e3410c0127
ankitjana <ankit92005@gmail.com>
2020-05-28 17:29:07 -0400

    Load Startup Data

--- pet-clinic-data/src/main/java/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/petclinic/services/map/OwnerServiceMap.java
@@ -2,10 +2,11 @@ package petclinic.services.map;
 
 import petclinic.model.Owner;
 import petclinic.services.CrudService;
+import petclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -32,5 +33,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public Owner findById(Long id) {
         return super.findById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
