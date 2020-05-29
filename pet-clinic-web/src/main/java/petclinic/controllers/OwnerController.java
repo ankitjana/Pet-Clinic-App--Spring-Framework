e29ec21e480c9873e856868533ddaae24e5e93a2
ankitjana <ankit92005@gmail.com>
2020-05-29 02:08:17 -0400

    Updated owner and vet view

--- pet-clinic-web/src/main/java/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/petclinic/controllers/OwnerController.java
@@ -1,12 +1,24 @@
 package petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import petclinic.services.OwnerService;
 
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
     @RequestMapping({"/owners","/owners/index.html","/owners/index"})
-    public String getOwner(){
+    public String getOwner(Model model){
+
+        model.addAttribute("owners",ownerService.findAll());
+
         return "owners/index";
     }
 }
