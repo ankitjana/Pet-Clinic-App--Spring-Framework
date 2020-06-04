64ff9494a856fe6c59c740d162f133deef567c60
ankitjana <ankit92005@gmail.com>
2020-06-04 16:21:54 -0400

    Added Repos

---
+++ pet-clinic-data/src/main/java/petclinic/repositories/PetTypeRepository.java
@@ -0,0 +1,7 @@
+package petclinic.repositories;
+
+import petclinic.model.PetType;
+import petclinic.services.CrudService;
+
+public interface PetTypeRepository extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/petclinic/repositories/VetRepository.java
@@ -0,0 +1,7 @@
+package petclinic.repositories;
+
+import petclinic.model.Vet;
+import petclinic.services.CrudService;
+
+public interface VetRepository extends CrudService<Vet, Long> {
+}
