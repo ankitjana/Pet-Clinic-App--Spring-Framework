6c6a3750d7f4f80e1cc2b84749d00c0a44bf6665
ankitjana <ankit92005@gmail.com>
2020-06-06 16:29:50 -0400

    Implement Lombok

--- pet-clinic-data/src/main/java/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/petclinic/model/Owner.java
@@ -1,10 +1,16 @@
 package petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "owners")
 public class Owner extends Person{
 
@@ -18,36 +24,16 @@ public class Owner extends Person{
     private String city;
     @Column(name = "telephone")
     private String telephone;
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String address, String city,
+                String telephone, Set<Pet> pets)
 
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
+            {
+        super(id, firstName, lastName);
         this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
         this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
         this.telephone = telephone;
+        this.pets = pets;
+
     }
 }
