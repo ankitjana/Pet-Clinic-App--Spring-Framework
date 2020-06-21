155440b1b9eb816065e73ba7e19739ea72883c5a
ankitjana <ankit92005@gmail.com>
2020-06-21 14:20:22 -0400

    Added Owner Details HTML

--- pet-clinic-web/src/main/java/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/petclinic/controllers/OwnerController.java
@@ -2,7 +2,10 @@ package petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import petclinic.services.OwnerService;
 
 @Controller
@@ -25,4 +28,12 @@ public class OwnerController {
     public String findOwners(){
         return "notImplemented";
     }
+
+    @GetMapping("owners/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId){
+        ModelAndView mav = new ModelAndView("/owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
+    }
+
 }
