3df367117f046eba3f4b918d6afb5f94498383f7
ankitjana <ankit92005@gmail.com>
2020-06-22 15:38:53 -0400

    Bug fix: Owner Controller

--- pet-clinic-web/src/main/java/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/petclinic/controllers/OwnerController.java
@@ -50,7 +50,7 @@ public class OwnerController {
             owner.setLastName("");
         }
 
-        List<Owner> results = ownerService.findAllByLastNameLike(owner.getLastName());
+        List<Owner> results = ownerService.findAllByLastNameLike("%"+owner.getLastName()+"%");
 
         if (results.isEmpty()){
             result.rejectValue("lastName","notFound","not found");
@@ -60,7 +60,7 @@ public class OwnerController {
             return "redirect:/owners/" + owner.getId();
         }else {
             model.addAttribute("selections",results);
-            return "owners/ownerList";
+            return "owners/ownersList";
 
         }
 
